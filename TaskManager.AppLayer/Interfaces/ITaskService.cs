ddcdc216c402b96aee99618233e5b0d25cf6d2ee
farhan <a.farhan79@gmail.com>
2026-06-28 12:41:53 +0200

    feat/applicationlayer-structure-&-services with interface and dto added

---
+++ TaskManager.AppLayer/Interfaces/ITaskService.cs
@@ -0,0 +1,26 @@
+﻿using TaskManager.Domain.Entities;
+using TaskManager.Domain.Enums;
+
+namespace TaskManager.AppLayer.Interfaces
+{
+    public interface ITaskService
+    {
+        public Task<TodoTask> CreateTaskAsync(
+           string title, 
+           string? description, 
+           Priority priority, 
+           DateTime dueDate, 
+           Guid? assignedToUserId, 
+           Guid? assignedByUserId);
+        public Task UpdateTaskAsync(TodoTask task);
+        public Task<TodoTask?> GetTaskByIdAsync(Guid taskId);
+        public Task<List<TodoTask>> GetAllTasksAsync();
+
+
+        public Task CompleteTaskAsync(Guid taskId);
+
+        public Task DeleteTaskAsync(Guid taskId);
+
+        public Task AssignTaskAsync(Guid taskId, Guid assignedToUserId, Guid assignedByUserId);
+    }
+}
